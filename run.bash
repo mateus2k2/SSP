56b3f38a495ae1ae3e822260ddb08278afe0ce13
Mateus Filipe <mateus.silva@petrosoftdesign.com>
2024-09-15 13:15:22 -0300

    update

--- run.bash
+++ run.bash
@@ -3,8 +3,9 @@ outputFolder=./output
 
 toolSetsFile=./input/ToolSetInt.csv
 
-for entry in "$instancesFolder"/*
+for entry in $(ls -v "$instancesFolder"/*)
 do
+    echo "$entry"
     filename=$(basename "$entry")
-    ./src/out/mainCpp $instancesFolder/$filename $toolSetsFile $outputFolder/$filename --TEMP_INIT 0.2 --TEMP_FIM 1 --N_REPLICAS 16 --MCL 400 --PTL 1000 --TEMP_DIST 1 --TYPE_UPDATE 2 --TEMP_UPDATE 35000
+    ./src/out/mainCpp "$instancesFolder/$filename" "$toolSetsFile" "$outputFolder/$filename" --TEMP_INIT 0.2 --TEMP_FIM 1 --N_REPLICAS 16 --MCL 400 --PTL 1000 --TEMP_DIST 1 --TYPE_UPDATE 2 --TEMP_UPDATE 35000
 done
