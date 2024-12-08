ba77b3fdefdfa68575b2927f53f43cb0271b7caf
Mateus <mateusfilipe2k2@gmail.com>
2024-12-08 11:28:05 -0300

    update

--- scripts/runAuto.sh
+++ scripts/runAuto.sh
@@ -4,57 +4,41 @@ outputFolder=./output/Exemplo
 [ ! -d "$outputFolder/MyInstancesSameToolSets" ] && mkdir -p "$outputFolder/MyInstancesSameToolSets"
 [ ! -d "$outputFolder/MyInstancesDiferentToolSets" ] && mkdir -p "$outputFolder/MyInstancesDiferentToolSets"
 
-make CompilePT DEBUGMODE=0
+make compilePT DEBUG_MODE=0
+
+run_instances() {
+    local instancesFolder=$1
+    local outputFolder=$2
+    local instanceMode=$3
+
+    echo "RODANDO INSTANCIAS DE $instancesFolder"
+    local counter=1
+    for entry in $(ls -v "$instancesFolder"/*.csv)
+    do
+        filename=$(basename "$entry")
+        echo "$counter ./src/out/mainCpp $instancesFolder/$filename $toolSetsFile $outputFolder/$filename"
+        ./src/out/mainCpp "$instancesFolder/$filename" "$toolSetsFile" "$outputFolder/$filename" \
+            --TEMP_INIT 0.2 \
+            --TEMP_FIM 1 \
+            --N_REPLICAS 16 \
+            --MCL 400 \
+            --PTL 100 \
+            --TEMP_DIST 1 \
+            --TYPE_UPDATE 2 \
+            --TEMP_UPDATE 35000 \
+            --INIT_SOL_TYPE 1 \
+            --RESULT_REPORT 1 \
+            --INSTANCE_MODE $instanceMode
+        counter=$((counter+1))
+    done
+}
 
 toolSetsFile=./input/Processed/ToolSetInt.csv
 
-#------------------------------------------------------------
-# MyInstancesSameToolSets
-#------------------------------------------------------------
-
+# Run instances for "MyInstancesSameToolSets"
 instancesFolder=./input/MyInstancesSameToolSets
+run_instances "$instancesFolder" "$outputFolder/MyInstancesSameToolSets" 1
 
-echo "RODANDO INSTANCIAS DE $instancesFolder"
-for entry in $(ls -v "$instancesFolder"/*.csv)
-do
-    echo "./src/out/mainCpp "$instancesFolder/$filename" "$toolSetsFile" "$outputFolder/MyInstancesSameToolSets/$filename""
-    filename=$(basename "$entry")
-    ./src/out/mainCpp "$instancesFolder/$filename" "$toolSetsFile" "$outputFolder/MyInstancesSameToolSets/$filename" \
-        --TEMP_INIT 0.2 \
-        --TEMP_FIM 1 \
-        --N_REPLICAS 16 \
-        --MCL 400 \
-        --PTL 100 \
-        --TEMP_DIST 1 \
-        --TYPE_UPDATE 2 \
-        --TEMP_UPDATE 35000 \
-        --INIT_SOL_TYPE 1 \
-        --RESULT_REPORT 1 \
-        --INSTANCE_MODE 1
-done
-
-#------------------------------------------------------------
-# MyInstancesDiferentToolSets
-#------------------------------------------------------------
-
+# Run instances for "MyInstancesDiferentToolSets"
 instancesFolder=./input/MyInstancesDiferentToolSets
-
-outputFolder=./output/Ajuste2-PTL100/MyInstancesDiferentToolSets
-echo "RODANDO INSTANCIAS DE $instancesFolder"
-for entry in $(ls -v "$instancesFolder"/*.csv)
-do
-    echo "$entry"
-    filename=$(basename "$entry")
-    ./src/out/mainCpp "$instancesFolder/$filename" "$toolSetsFile" "$outputFolder/MyInstancesDiferentToolSets/$filename" \
-        --TEMP_INIT 0.2  \
-        --TEMP_FIM 1  \
-        --N_REPLICAS 16  \
-        --MCL 400  \
-        --PTL 100  \
-        --TEMP_DIST 1  \
-        --TYPE_UPDATE 2  \
-        --TEMP_UPDATE 35000 \
-        --INIT_SOL_TYPE 1 \
-        --RESULT_REPORT 1 \
-        --INSTANCE_MODE 0
-done
+run_instances "$instancesFolder" "$outputFolder/MyInstancesDiferentToolSets" 0
