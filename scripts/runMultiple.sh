ed4e2791aed18c7da95590d9db810939ead089bd
Mateus <mateusfilipe2k2@gmail.com>
2025-02-02 16:06:47 -0300

    update

--- scripts/runMultiple.sh
+++ scripts/runMultiple.sh
@@ -4,5 +4,5 @@ runMode=${2:-"both"}
 for i in $(seq 1 10)
 do
     echo "Running $i"
-    ./scripts/runAuto.sh $outputFolder/$i diferent > out
+    ./scripts/runAuto.sh $outputFolder/$i $runMode
 done
