6492436e0cae6e35064d69c88e9aab8273c7f3b6
Mateus <mateusfilipe2k2@gmail.com>
2025-02-24 13:26:41 -0300

    update

---
+++ convergencia.sh
@@ -0,0 +1,137 @@
+echo "PTL = 10"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 10 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 100"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 100 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 200"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 200 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 400"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 400 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 500"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 500 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 600"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 600 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 1000"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 1000 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+echo "PTL = 2000"
+./src/out/mainCpp "./input/MyInstancesSameToolSets/n=1000,p=0.25,r=0.6,t=3950,v18.csv" "./input/Processed/ToolSetInt.csv" "./output/Exemplo/exemplo.txt" \
+		--TEMP_INIT 0.1 \
+		--TEMP_FIM 5 \
+		--N_REPLICAS 11 \
+		--MCL 500 \
+		--PTL 2000 \
+		--TEMP_DIST 3 \
+		--TYPE_UPDATE 1 \
+		--INIT_SOL_TYPE 0 \
+		--TEMP_UPDATE 3500 \
+		--PTL_TEMP_UPDATE_PROPORTION 3 \
+		--RESULT_REPORT 1 \
+		--DIFERENT_TOOLSETS_MODE 0 
+echo ""
+
+10 11576 37242
+100 13086 405121
+200 13640 835461
+400 13780 1755362
+500 13888 2199493
+600 13975 2628898
+1000 14481 8802468
+2000 14662 9904422
+
